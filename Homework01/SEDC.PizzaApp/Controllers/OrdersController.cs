ffd6a282c70b94311950a2a60729b970eb15dc3e
MilovskiAleksandar <119674347+MilovskiAleksandar@users.noreply.github.com>
2023-06-11 01:05:23 +0200

    Homework01

---
+++ Homework01/SEDC.PizzaApp/Controllers/OrdersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        //Add Index action that returns a view with a simple html that says “List of orders”.
+        //It should be accessed through custom route http://localhost:[port]/ListOrders
+        [Route("ListOrders")]
+        //I change it to show List of all Orders in the html :)
+        public IActionResult Index()
+        {
+            List<Orders> orders = StaticDb.Orders;
+            return View(orders);
+        }
+        //Add Details action with optional parameter id. If id is null it should return empty action result
+        //, else it should return a view with simple html.
+        //It should be accessed through route http://localhost/[Controller Name]/Details/{id?}
+
+        //COMMENT BECAUSE FOR BONUS I NEED TO CHANGE IT TO THE CODE BELOW
+
+        //public IActionResult Details(int? id)
+        //{
+        //    if(id == null)
+        //    {
+        //        return new EmptyResult();
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+        //}
+        //Add an action that returns Json (create an example model, class by your choice)
+        //it should be accessed by http://localhost/[Controller Name]/JsonData.
+        public IActionResult JsonData()
+        {
+            List<Orders> orders = StaticDb.Orders.ToList();
+            return new JsonResult(orders);
+        }
+        //Add an action that redirects to Action Index in Home Controller.
+        public IActionResult RedirectToHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        //Create model for Order and static list consisting of several orders.
+        //Use it in the Details action (refer to the demo for hint)
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Error", "Orders");
+            }
+
+            Orders order = StaticDb.Orders.FirstOrDefault(o => o.Id == id);
+            if(order == null)
+            {
+                return RedirectToAction("Error", "Orders");
+            }
+
+            return View(order);
+        }
+
+
+    }
+}
---
+++ Homework01/SEDC.PizzaApp/StaticDb.cs
@@ -0,0 +1,57 @@
+﻿using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp
+{
+    public static class StaticDb
+    {
+        public static List<Pizza> Pizzas = new List<Pizza>()
+        {
+            new Pizza()
+            {
+                Id = 1,
+                Name = "Capri",
+                Price = 350,
+                IsOnPromotion = true,
+            },
+
+            new Pizza()
+            {
+                Id = 2,
+                Name = "Pepperoni",
+                Price = 400,
+                IsOnPromotion = false,
+            },
+        };
+
+        public static List<Orders> Orders = new List<Orders>()
+        {
+            new Orders()
+            {
+                Id = 1,
+                Price = 300,
+                IsDelivered = false,
+                CustomerName = "Filip",
+                OrderDate = new DateTime(2021, 6, 1)
+                
+            },
+            new Orders()
+            {
+                Id = 2,
+                Price = 350,
+                IsDelivered = true,
+                CustomerName = "Kristijan",
+                OrderDate = new DateTime(2022, 8, 10)
+            },
+
+            new Orders()
+            {
+                Id = 3,
+                Price = 230,
+                IsDelivered = true,
+                CustomerName = "Marija",
+                OrderDate = new DateTime(2020, 2, 16)
+            }
+
+        };
+    }
+}
